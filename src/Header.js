e661ed7eec7f86497d1966d5de20eca7ac425369
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-09-21 10:40:12 +0530

    Finished the header part

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+
+function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header_icon" />
+      </IconButton>
+
+      <img
+        className="header_logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="tinder"
+      />
+
+      <IconButton>
+        <QuestionAnswerIcon fontSize="large" className="header_icon" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default Header;
